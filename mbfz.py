b54c36090f27446c07b95889658a35c634f0a53d
Hikmat <135993585+HikmatXF@users.noreply.github.com>
2023-06-08 22:55:16 +0700

    Add files via upload

--- mbfz.py
+++ mbfz.py
@@ -38,6 +38,7 @@ try:
 except:pass
 try:
 	import requests
+	from requests.exceptions import ChunkedEncodingError
 	from bs4 import BeautifulSoup as sop
 	from concurrent.futures import ThreadPoolExecutor as tread
 except:
@@ -80,57 +81,62 @@ logoku = """
 
 # * [ SOURCE LOGIN COOKIE  ] * #
 def loginCookies(PukimakBangetKamuAnjink):
-	os.system("clear")
-	cookie_anda = input(" [?] cookie : ")
+	sis("clear")
+	cookie = input(" [?] cookie : ")
 	try:
-		requests.Session().headers.update({'content-type': 'application/x-www-form-urlencoded'})
+		ses = requests.Session()
+		ses.headers.update({'content-type': 'application/x-www-form-urlencoded'})
 		data = {'access_token': '1348564698517390|007c0a9101b9e1c8ffab727666805038','scope': ''}
-		response = json.loads(requests.Session().post('https://graph.facebook.com/v2.6/device/login/', data = data).text)
+		response = json.loads(ses.post('https://graph.facebook.com/v2.6/device/login/', data = data).text)
 		code, user_code = response['code'], response['user_code']
 		verification_url, status_url = ('https://m.facebook.com/device?user_code={}'.format(user_code)), ('https://graph.facebook.com/v2.6/device/login_status?method=post&code={}&access_token=1348564698517390%7C007c0a9101b9e1c8ffab727666805038&callback=LeetsharesCallback'.format(code))
-		requests.Session().headers.pop('content-type')
-		requests.Session().headers.update({'sec-fetch-mode': 'navigate','user-agent': 'Mozilla/5.0 (Linux; Android 9; RMX1941 Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.54 Mobile Safari/537.36','sec-fetch-site': 'cross-site','Host': 'm.facebook.com','accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','sec-fetch-dest': 'document',})
-		response2 = requests.Session().get(verification_url, cookies = {'cookie': cookie_anda}).text
+		ses.headers.pop('content-type')
+		ses.headers.update({'sec-fetch-mode': 'navigate','user-agent': 'Mozilla/5.0 (Linux; Android 9; RMX1941 Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/107.0.5304.54 Mobile Safari/537.36','sec-fetch-site': 'cross-site','Host': 'm.facebook.com','accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','sec-fetch-dest': 'document',})
+		response2 = ses.get(verification_url, cookies = {'cookie': cookie}).text
 		if 'Bagaimana Anda ingin masuk ke Facebook?' in str(response2) or '/login/?next=' in str(response2):
-			print(" [!] cookie anda invalid ")
+			print(" [?] cookie anda invalid ")
 			exit()
 		else:
 			action = re.search('action="(.*?)">', str(response2)).group(1).replace('amp;', '')
 			data = {'fb_dtsg': re.search('name="fb_dtsg" value="(.*?)"', str(response2)).group(1),'jazoest': re.search('name="jazoest" value="(\d+)"', str(response2)).group(1),'qr': 0,'user_code': response['user_code']}
-			requests.Session().headers.update({'origin': 'https://m.facebook.com','referer': 'https://m.facebook.com/device?user_code={}'.format(user_code),'content-type': 'application/x-www-form-urlencoded','sec-fetch-site': 'same-origin',})
-			response3 = requests.Session().post('https://m.facebook.com{}'.format(action), data = data, cookies = {'cookie': cookie_anda})
+			ses.headers.update({'origin': 'https://m.facebook.com','referer': 'https://m.facebook.com/device?user_code={}'.format(user_code),'content-type': 'application/x-www-form-urlencoded','sec-fetch-site': 'same-origin',})
+			response3 = ses.post('https://m.facebook.com{}'.format(action), data = data, cookies = {'cookie': cookie})
 			if 'https://m.facebook.com/dialog/oauth/?auth_type=rerequest&redirect_uri=' in str(response3.url):
-				requests.Session().headers.pop('content-type');requests.Session().headers.pop('origin')
-				response4 = requests.Session().post(response3.url, data = data, cookies = {'cookie': cookie_anda}).text
+				ses.headers.pop('content-type');ses.headers.pop('origin')
+				response4 = ses.post(response3.url, data = data, cookies = {'cookie': cookie}).text
 				action = re.search('action="(.*?)"', str(response4)).group(1).replace('amp;', '')
-				requests.Session().headers.update({'origin': 'https://m.facebook.com','referer': response3.url,'content-type': 'application/x-www-form-urlencoded',})
+				ses.headers.update({'origin': 'https://m.facebook.com','referer': response3.url,'content-type': 'application/x-www-form-urlencoded',})
 				data = {'fb_dtsg': re.search('name="fb_dtsg" value="(.*?)"', str(response4)).group(1),'jazoest': re.search('name="jazoest" value="(\d+)"', str(response4)).group(1),'scope': re.search('name="scope" value="(.*?)"', str(response4)).group(1),'display': re.search('name="display" value="(.*?)"', str(response4)).group(1),'user_code': re.search('name="user_code" value="(.*?)"', str(response4)).group(1),'logger_id': re.search('name="logger_id" value="(.*?)"', str(response4)).group(1),'auth_type': re.search('name="auth_type" value="(.*?)"', str(response4)).group(1),'encrypted_post_body': re.search('name="encrypted_post_body" value="(.*?)"', str(response4)).group(1),'return_format[]': re.search('name="return_format\\[\\]" value="(.*?)"', str(response4)).group(1)}
-				response5 = requests.Session().post('https://m.facebook.com{}'.format(action), data = data, cookies = {'cookie': cookie_anda}).text
+				response5 = ses.post('https://m.facebook.com{}'.format(action), data = data, cookies = {'cookie': cookie}).text
 				windows_referer = re.search('window.location.href="(.*?)"', str(response5)).group(1).replace('\\', '')
-				requests.Session().headers.pop('content-type');requests.Session().headers.pop('origin')
-				requests.Session().headers.update({'referer': 'https://m.facebook.com/',})
-				response6 = requests.Session().get(windows_referer, cookies = {'cookie': cookie_anda}).text
+				ses.headers.pop('content-type');ses.headers.pop('origin')
+				ses.headers.update({'referer': 'https://m.facebook.com/',})
+				response6 = ses.get(windows_referer, cookies = {'cookie': cookie}).text
 				if "Sukses!" in str(response6):
-					requests.Session().headers.update({'sec-fetch-mode': 'no-cors','referer': 'https://graph.facebook.com/','Host': 'graph.facebook.com','accept': '*/*','sec-fetch-dest': 'script','sec-fetch-site': 'cross-site',})
-					response7 = requests.Session().get(status_url, cookies = {'cookie': cookie_anda}).text
+					ses.headers.update({'sec-fetch-mode': 'no-cors','referer': 'https://graph.facebook.com/','Host': 'graph.facebook.com','accept': '*/*','sec-fetch-dest': 'script','sec-fetch-site': 'cross-site',})
+					response7 = ses.get(status_url, cookies = {'cookie': cookie}).text
 					access_token = re.search('"access_token": "(.*?)"', str(response7)).group(1)
-					print(" [√] cookie anda valid ")
-					turu(2)
-					open("cookie.txt","w").write(cookie_anda);open("token.txt","w").write(access_token);utamaMenu("Masa Sih")
+					print(" [√] cookie valid ")
+					sis("clear")
+					open("cookie.txt","w").write(cookie);open("token.txt","w").write(access_token);utamaMenu("NgentodAsu")
 				else:
-					print(" [!] kesalahan saat convert token ")
+					print(" [!] gagal convert ke token ")
 					exit()
 	except (ConnectionError, ChunkedEncodingError):
-		print(" [!] sinyal anda bermasalah ")
+		print(" [?] koneksi anda bermasalah ")
 		exit()
 
 # * [ MENU SCRIPT MBFz ] * #
-def menu(Hayyuk):
+def utamaMenu(Hayyuk):
 	try:sinyalmu = "koneksi anda aman/menggunakan data"
 	except requests.exceptions.ConnectionError:sinyalmu = "anda sedang tidak menggunakan data/koneksi gangguan"
 	try:
 		cookie = {"cookie":open("cookie.txt","r").read()}
 		token = open("token.txt","r").read()
+	except IOError:
+		sis("rm -rf cookie.txt && rm -rf token.txt")
+		loginCookies("Bangke")
+	try:
 		ambil  = requests.Session().get('https://graph.facebook.com/me?fields=name,id&access_token=%s'%(token),cookies=cookie)
 		mberr = json.loads(ambil.text)
 		try:nama = mberr["name"]
@@ -295,5 +301,5 @@ if __name__ in "__main__":
 			os.mkdir("OK")
 		except:pass
 	except:pass
-	menu("Coba Aja")
+	utamaMenu("Coba Aja")
 	
